0048ec18e25947d0622623a2d9e5f6d3c45813f6
Vahan Levonyan <v_levonyan@yahoo.com>
2016-07-07 19:28:36 +0400

    Finished Beatbox interface

---
+++ BeatBox/app/src/main/java/com/egs/vahanl/beatbox/Sound.java
@@ -0,0 +1,24 @@
+package com.egs.vahanl.beatbox;
+
+/**
+ * Created by vahanl on 7/7/16.
+ */
+public class Sound {
+    private String mAssetPath;
+    private String mName;
+
+    public Sound(String assetPath) {
+        mAssetPath = assetPath;
+        String[] components = assetPath.split("/");
+        String filename = components[components.length - 1];
+        mName = filename.replace(".wav", "");
+    }
+
+    public String getmAssetPath() {
+        return mAssetPath;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+}

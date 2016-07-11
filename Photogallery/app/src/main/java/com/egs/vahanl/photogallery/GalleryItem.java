af10b15f9eadeab9831092db027d641a9b251061
Vahan Levonyan <v_levonyan@yahoo.com>
2016-07-11 15:30:53 +0400

    Added PhotoGallery

---
+++ Photogallery/app/src/main/java/com/egs/vahanl/photogallery/GalleryItem.java
@@ -0,0 +1,40 @@
+package com.egs.vahanl.photogallery;
+
+/**
+ * Created by vahanl on 7/11/16.
+ */
+public class GalleryItem {
+
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+
+    @Override
+    public String toString() {
+        return mCaption;
+    }
+
+    public String getCaption() {
+        return mCaption;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setCaption(String caption) {
+        mCaption = caption;
+    }
+
+    public void setId(String id) {
+        mId = id;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
+}

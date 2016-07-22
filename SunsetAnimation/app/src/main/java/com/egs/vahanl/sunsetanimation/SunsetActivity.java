a0f09139b179ed423157029a06e0523d2e30f979
Vahan Levonyan <v_levonyan@yahoo.com>
2016-07-22 18:19:42 +0400

    Added Sunset project

---
+++ SunsetAnimation/app/src/main/java/com/egs/vahanl/sunsetanimation/SunsetActivity.java
@@ -0,0 +1,19 @@
+package com.egs.vahanl.sunsetanimation;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SunsetActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return SunsetFragment.newInstance();
+    }
+}
